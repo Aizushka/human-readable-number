c9d84f9f01e76fb776257a027b1e7df49f83f1d8
Ayza <7302512@gmail.com>
2023-01-12 15:24:46 -0500

    fix spelling mistakes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ module.exports = function toReadable (number) {
     switch (number) {
         case 10: return "ten"
         case 11: return "eleven";
-        case 12: return "twelwe";
-        case 13: return "thireteen";
+        case 12: return "twelve";
+        case 13: return "thirteen";
         case 14: return "fourteen"; 
         case 15: return "fifteen";
         case 16: return "sixteen";
         case 17: return "seventeen";
         case 18: return "eighteen";
-        case 19: return "ninteen";
+        case 19: return "nineteen";
     }
     
     let digit = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
@@ -22,5 +22,5 @@ module.exports = function toReadable (number) {
         return digit[number];
     }
 
-    
+
 }
