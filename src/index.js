3ae5aafcffc2b9ddc87096cf725a93f937e455e7
Ayza <7302512@gmail.com>
2023-01-12 14:03:05 -0500

    init: project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function toReadable (number) {
-  
+    switch (number) {
+        case 10: return "ten"
+        case 11: return "eleven";
+        case 12: return "twelwe";
+        case 13: return "thireteen";
+        case 14: return "fourteen"; 
+        case 15: return "fifteen";
+        case 16: return "sixteen";
+        case 17: return "seventeen";
+        case 18: return "eighteen";
+        case 19: return "ninteen";
+    }
+    
+    let digit = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let dozens = [ 'twenty', 'thirty', 'fourty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    let numberString = number.toString();
+    let dec = numberString.length;
+    
+    if (dec == 1) {
+        return digit[number];
+    }
+
+    
 }
